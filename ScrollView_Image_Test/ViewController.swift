28c39cb8b882e9f48fab51d4e23afee9599cd439
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-09-28 18:01:16 -0400

    - Added picker view with basic info

--- ScrollView_Image_Test/ViewController.swift
+++ ScrollView_Image_Test/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var collection = Array(repeating: 1, count: 3)
+    
     override func viewDidLoad() {
+        pickerView.delegate = self
+        pickerView.dataSource = self
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let item = collection[row]
+        return ("\(item.description)")
+    }
+}
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return collection.count
+    }
+    
+    
+}
