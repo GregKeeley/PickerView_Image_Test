c35129a46d1dab5725259dac4fc33704b66a22e4
Gregory Keeley <gregoryKeeley@pursuit.org>
2020-09-28 17:48:49 -0400

    Initial Commit

---
+++ ScrollView_Image_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollView_Image_Test
+//
+//  Created by Gregory Keeley on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScrollView_Image_TestTests/ScrollView_Image_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  ScrollView_Image_TestTests.swift
+//  ScrollView_Image_TestTests
+//
+//  Created by Gregory Keeley on 9/28/20.
+//
+
+import XCTest
+@testable import ScrollView_Image_Test
+
+class ScrollView_Image_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
